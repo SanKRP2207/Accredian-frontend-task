38bae197599229cb147117cd1057d6376eb76447
SanKRP2207 <sandeepkumarpal281@gmail.com>
2024-07-16 00:02:24 +0530

    last commitdone

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://accredian-backend-task-yzck.onrender.com';
 
 export const createReferral = async (referralData) => {
   try {
